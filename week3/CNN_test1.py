55010d99bf15df774d739de65b22f77346801a23
Pascal Bourgoin <pascal680@hotmail.ca>
2021-09-20 13:31:14 -0400

    Completion du CNN 2D pour la categorisation de chiffre du dataset minst avec un model minimal optimiser

--- week3/CNN_test1.py
+++ week3/CNN_test1.py
@@ -9,13 +9,20 @@ mnist = tf.keras.datasets.mnist #28x28 hand written images 0-9
 x_train = tf.keras.utils.normalize(x_train, axis=1)
 x_test = tf.keras.utils.normalize(x_test, axis=1)
 
+print(x_train.shape)
+print(y_train.shape)
+
+# Reshape data 
+x_train = x_train.reshape(len(x_train), 28, 28, 1)
+x_test  = x_test.reshape(len(x_test), 28, 28, 1)
+
 model = tf.keras.models.Sequential()
-model.add(Conv2D(32, (3, 3), activation = 'relu', input_shape=(28, 28, 1)))
-model.add(MaxPooling2D((2, 2)))
-model.add(Conv2D(32, (3, 3), activation = 'relu', input_shape=(28, 28, 1)))
+model.add(Conv2D(4, (3, 3), activation = 'relu', input_shape=(28, 28, 1)))
+# model.add(MaxPooling2D((2, 2)))
+# model.add(Conv2D(32, (3, 3), activation = 'relu', input_shape=(28, 28, 1)))
 model.add(MaxPooling2D((2, 2)))
 model.add(tf.keras.layers.Flatten())
-model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(32, activation=tf.nn.relu))
 model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))
 
 model.compile(optimizer= 'adam',
@@ -35,7 +42,7 @@ new_model = tf.keras.models.load_model("num_reader_cnn.model")
 
 predictions = new_model.predict([x_test])
 
-# print(predictions)
+print(predictions)
 
 index = 0
 
