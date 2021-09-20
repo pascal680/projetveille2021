b87895d2f77b162e87feff56e918ead18a52e73e
Pascal Bourgoin <pascal680@hotmail.ca>
2021-09-20 03:29:54 -0400

    Essaie d'un CNN mais pour la detection de chiffre ecris a la main, reste a regler une erreur

--- week3/CNN_test1.py
+++ week3/CNN_test1.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
 
 mnist = tf.keras.datasets.mnist #28x28 hand written images 0-9
 
@@ -8,29 +10,34 @@ x_train = tf.keras.utils.normalize(x_train, axis=1)
 x_test = tf.keras.utils.normalize(x_test, axis=1)
 
 model = tf.keras.models.Sequential()
+model.add(Conv2D(32, (3, 3), activation = 'relu', input_shape=(28, 28, 1)))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(32, (3, 3), activation = 'relu', input_shape=(28, 28, 1)))
+model.add(MaxPooling2D((2, 2)))
 model.add(tf.keras.layers.Flatten())
-model.dd(tf.keras.layers.Dense(128, activation=tf.nn.relu))
 model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
 model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))
 
-model.compile(optimizer= 'adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer= 'adam',
+                loss='sparse_categorical_crossentropy',
+                metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=10)
+model.fit(x_train, y_train, epochs=6)
 
 
 val_loss, val_acc = model.evaluate(x_test, y_test)
 print(val_loss, val_acc)
 
 
-model.save('num_reader.model')
+model.save('num_reader_cnn.model')
 
-new_model = tf.keras.models.load_model("num_reader.model")
+new_model = tf.keras.models.load_model("num_reader_cnn.model")
 
 predictions = new_model.predict([x_test])
 
 # print(predictions)
 
-index = 10
+index = 0
 
 import numpy as np
 print(np.argmax(predictions[index])) #uses import numpy to take highest probability number in tensor full of predictions to give accurate answer as to which number it is
