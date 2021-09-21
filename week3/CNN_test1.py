f8a398a1a9f3d9c38649e725a35e756bcc183dcf
Pascal Bourgoin <pascal680@hotmail.ca>
2021-09-21 02:39:54 -0400

    Creer un CNN non optimiser qui permet de classifier des images dans des categories predefinies, reste a optimiser et essayer avec different datasets

--- week3/CNN_test1.py
+++ week3/CNN_test1.py
@@ -18,8 +18,6 @@ x_test  = x_test.reshape(len(x_test), 28, 28, 1)
 
 model = tf.keras.models.Sequential()
 model.add(Conv2D(4, (3, 3), activation = 'relu', input_shape=(28, 28, 1)))
-# model.add(MaxPooling2D((2, 2)))
-# model.add(Conv2D(32, (3, 3), activation = 'relu', input_shape=(28, 28, 1)))
 model.add(MaxPooling2D((2, 2)))
 model.add(tf.keras.layers.Flatten())
 model.add(tf.keras.layers.Dense(32, activation=tf.nn.relu))
@@ -29,7 +27,7 @@ model.compile(optimizer= 'adam',
                 loss='sparse_categorical_crossentropy',
                 metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=6)
+model.fit(x_train, y_train, epochs=10)
 
 
 val_loss, val_acc = model.evaluate(x_test, y_test)
@@ -44,15 +42,16 @@ predictions = new_model.predict([x_test])
 
 print(predictions)
 
-index = 0
+
+index = 19
 
 import numpy as np
 print(np.argmax(predictions[index])) #uses import numpy to take highest probability number in tensor full of predictions to give accurate answer as to which number it is
 
-import matplotlib.pyplot as plt
+print("actual value " , y_test[index]) #prints actual value meant to be predicted
 
-# plt.imshow(x_train[0], cmap=plt.cm.binary) #shows image 1 at tensor 1 in binary colormap
+import matplotlib.pyplot as plt #Permet la visualisation de l'image 28x28 de x_test sur laquelle le convolutional neural network fait son estimation
+
 
 plt.imshow(x_test[index])
 plt.show()
-# print(x_train[0])
---
+++ week3/CNN_test2.py
@@ -0,0 +1,81 @@
+# keras imports for the dataset and building our neural network
+from keras.datasets import cifar10
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Conv2D, MaxPool2D, Flatten
+from keras.utils import np_utils
+import tensorflow as tf
+
+
+
+# loading the dataset
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+print(x_train.shape)
+# # building the input vector from the 32x32 pixels
+x_train = x_train.reshape(x_train.shape[0], 32, 32, 3)
+x_test = x_test.reshape(x_test.shape[0], 32, 32, 3)
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+
+# normalizing the data to help with the training
+x_train /= 255
+x_test /= 255
+
+# one-hot encoding using keras' numpy-related utilities
+nb_classes = 10
+print("Shape before one-hot encoding: ", y_train.shape)
+y_train = np_utils.to_categorical(y_train, nb_classes)
+y_test = np_utils.to_categorical(y_test, nb_classes)
+print("Shape after one-hot encoding: ", y_train.shape)
+
+# building a linear stack of layers with the sequential model
+model = Sequential()
+
+
+# convolutional layer
+model.add(Conv2D(70, kernel_size=(3,3), strides=(1,1), padding='same', activation='relu')) #Applies filter on the image in squares of 3x3 matrices and adding it all up with an activation function to reinforce patterns
+model.add(MaxPool2D(pool_size=(2,2))) #Compress filtered data for image and reinforce patterns
+model.add(Dropout(0.25)) #Remove useless filtered images and reduce training time
+
+model.add(Conv2D(70, kernel_size=(3,3), strides=(1,1), padding='same', activation='relu')) #Applies filter on the image in squares of 3x3 matrices and adding it all up with an activation function to reinforce patterns
+model.add(MaxPool2D(pool_size=(2,2))) #Compress filtered data for image and reinforce patterns
+model.add(Dropout(0.25)) #Remove useless filtered images and reduce training time
+
+# flatten output of conv
+model.add(Flatten()) #Flattens all tensor with all the 
+
+# hidden layer
+model.add(Dense(400, activation='relu')) #Learning from the reinforced patterns in the convolutional layers
+model.add(Dropout(0.4)) #Heavy dropout rate to increase training speed and drop the useless neurons 
+model.add(Dense(200, activation='relu'))
+model.add(Dropout(0.3))
+# output layer
+model.add(Dense(nb_classes, activation='softmax')) #Outputs the category in which the image belongs too according to the current state of the CNN
+
+# compiling the sequential model
+model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer='adam')
+
+# training the model for 10 epochs
+model.fit(x_train, y_train, batch_size=128, epochs=10, validation_data=(x_test, y_test))
+
+model.save('image_categorizing_cnn.model')
+
+new_model = tf.keras.models.load_model("num_reader_cnn.model")
+
+predictions = new_model.predict([x_test])
+
+print(predictions)
+
+
+index = 2
+
+import numpy as np
+print(np.argmax(predictions[index])) #uses import numpy to take highest probability number in tensor full of predictions to give accurate answer as to which number it is
+
+print("actual value " , y_test[index]) #prints actual value meant to be predicted
+
+import matplotlib.pyplot as plt #Permet la visualisation de l'image 28x28 de x_test sur laquelle le convolutional neural network fait son estimation
+
+
+plt.imshow(x_test[index])
+plt.show()
