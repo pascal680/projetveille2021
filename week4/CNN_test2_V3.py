8f437288689f362b350b8224b83cd349f43a1166
Pascal Bourgoin <pascal680@hotmail.ca>
2021-09-30 16:33:21 -0400

    Ajout de neurone dans le dense layer pour creer plus de variation de fonction et donc rendre l'entrainement plus efficace. Preque 92% dans 25 epochs

--- week4/CNN_test2_V3.py
+++ week4/CNN_test2_V3.py
@@ -34,11 +34,11 @@ model = Sequential()
 
 model.add(Conv2D(50, kernel_size=(3,3), strides=(1,1), padding='same', activation='relu', input_shape=(32, 32, 3)))
 # convolutional layer
-model.add(Conv2D(75, kernel_size=(5,5), strides=(2,2), padding='same', activation='relu')) #Applies filter on the image in squares of 3x3 matrices and adding it all up with an activation function to reinforce patterns
+model.add(Conv2D(75, kernel_size=(3,3), strides=(1,1), padding='same', activation='relu')) #Applies filter on the image in squares of 3x3 matrices and adding it all up with an activation function to reinforce patterns
 model.add(MaxPool2D(pool_size=(2,2))) #Compress filtered data for image and reinforce patterns
 model.add(Dropout(0.25)) #Remove useless filtered images and reduce training time
 
-model.add(Conv2D(125, kernel_size=(5,5), strides=(2,2), padding='same', activation='relu')) #Applies filter on the image in squares of 3x3 matrices and adding it all up with an activation function to reinforce patterns
+model.add(Conv2D(125, kernel_size=(3,3), strides=(1,1), padding='same', activation='relu')) #Applies filter on the image in squares of 3x3 matrices and adding it all up with an activation function to reinforce patterns
 model.add(MaxPool2D(pool_size=(2,2))) #Compress filtered data for image and reinforce patterns
 model.add(Dropout(0.25)) #Remove useless filtered images and reduce training time
 
@@ -46,9 +46,9 @@ model.add(Dropout(0.25)) #Remove useless filtered images and reduce training tim
 model.add(Flatten()) #Flattens all tensor with all the 
 
 # hidden layer
-model.add(Dense(400, activation='relu')) #Learning from the reinforced patterns in the convolutional layers
+model.add(Dense(450, activation='relu')) #Learning from the reinforced patterns in the convolutional layers
 model.add(Dropout(0.4)) #Heavy dropout rate to increase training speed and drop the useless neurons 
-model.add(Dense(200, activation='relu'))
+model.add(Dense(250, activation='relu'))
 model.add(Dropout(0.3))
 # output layer
 model.add(Dense(nb_classes, activation='softmax')) #Outputs the category in which the image belongs too according to the current state of the CNN
@@ -59,9 +59,9 @@ model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer='
 # training the model for 10 epochs
 model.fit(x_train, y_train, batch_size=128, epochs=25, validation_data=(x_test, y_test))
 
-model.save('image_categorizing_cnn_v3.1.model')
+model.save('image_categorizing_cnn_v3.2.model')
 
 # CONCLUSION
-# Ajout des strides dans les couches conv2d mais cause une perte de precision et un perte des patterns car les filtres
-# du conv2d on moins d'effet sur une image de tel grandeur avec les strides et les plus gros filtres
+# Ajout de neurone dans la couches dense du modele sur le modele optimiser avec les couches conv2d pour permettre un entrainement plus efficace
+# et permettre les liens et la fonction d'etre ajuster plus rapidement
 # Diminution, rendu a 80% apres 25 epochs. Stride non utile pour image 32x32
